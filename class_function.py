eb3b0e744fd014e8beda9743133189396df3b2da
marcopavon <marco.pavoni19@yahoo.com>
2022-04-02 13:27:11 +0200

    rename file

--- class_function.py
+++ class_function.py
@@ -1,9 +1,11 @@
 import time
 import datetime
+import os
 
 
 class Timer:
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time_miliseconds = datetime.datetime.now()
 
     def __repr__(self):
         return "Test()"
@@ -11,10 +13,32 @@ class Timer:
         return f"All we got is {self.current_time}"
     
     def return_list(self):
-        return [self.current_time, "locker"]
+        return [self.current_time, "locker", self.current_time_miliseconds]
+
+
+class Csv:
+    csv_file =  "../Sandbox/test_file.csv"
+
+    def read_out(self):
+        f1 = open(self.csv_file, "r")
+        return f1.readlines()[-3:]
+
+    def read_out_all(self):
+        f1 = open(self.csv_file, "r")
+        return f1.readlines()
+
+    def clean_data(self, input):
+        splitt_list = input.split(" ")
+        return splitt_list[0]
 
 
 
+    def read_out_element(self):
+        for x in self.read_out():
+            print(self.clean_data(x))
 
 
+new_file = Csv()
+new_file.read_out_element()
 
+#print(f"This is the current directory, which you are in: {os.getcwd()}")
