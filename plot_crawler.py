0b0e57de9e11aae4d33e0a8ab86e2fb1f4f9dea1
marcopavon <marco.pavoni19@yahoo.com>
2022-08-19 08:26:24 +0200

    Update

--- plot_crawler.py
+++ plot_crawler.py
@@ -1,10 +1,18 @@
 # Import the necessary modules
 from asyncore import read
 from turtle import title
+import matplotlib
 import matplotlib.pyplot as plt
 from numpy import NaN
 import pandas as pd
 import re
+import datetime
+import os, sys
+from  builtins import any
+
+
+#personal lib
+
 
 """
 from test_regex import update_csv_output
@@ -58,6 +66,9 @@ df['Ranking'] = pd.to_numeric(df['Ranking'])
 df[' Prime'] = pd.to_numeric(df[' Prime'])
 df[' Date'] = pd.to_datetime(df[' Date'])
 df['Week'] = df[' Date'].dt.week.astype("Int64")
+df['Week day'] = df[' Date'].dt.weekday.astype("Int64")
+df['Month day'] = df[' Date'].dt.day.astype("Int64")
+df['Month'] = df[' Date'].dt.month.astype("Int64")
 df[' Date with time'] = pd.to_datetime(df[' Date with time'])
 df[' Postcode'] = df[' Postcode'].astype("Int64")
 df[' Year of birth'] = df[' Year of birth'].astype("Int64")
@@ -83,13 +94,16 @@ print(df)
 data_mean = df_delay.groupby(["Week"])[" Duration ELVIA"].mean()
 print(data_mean[:10])
 
-print(set(df[" Date"]))
+#print(set(df[" Date"]))
 
 
-fig, axes = plt.subplots(nrows=2, ncols=3)
+fig, axes = plt.subplots(nrows=3, ncols=3)
 plt.subplots_adjust(hspace = 0.51)
 
 fig.suptitle('Crawler Analysis Comparis')
+
+fig.set_size_inches(18.5, 10.5, forward=True)
+
 
 
 df.groupby(["Week"]).agg(
@@ -102,24 +116,30 @@ df.groupby(["Week"]).agg(
 df.groupby(["Week"]).agg(
     {
     #"Ranking" : "mean",
-    " Prime" : "mean"
+    " Prime" : "count"
     }
 ).plot(kind = "bar", ax=axes[0,1])
 
-df.groupby([" Postcode"]).agg(
+df.groupby([" Postcode", " Coverage"]).agg(
     {
     "Ranking" : "mean",
     #" Prime" : "mean"
     }
-).plot(kind = "bar", ax=axes[1,0])
+).unstack(' Coverage').plot(kind = "bar", ax=axes[1,0])
 
-df.groupby([" Coverage"]).agg(
+df.groupby(["Week", " Year of birth"]).agg(
     {
     "Ranking" : "mean",
     #" Prime" : "mean"
     }
-).plot(kind = "bar", ax=axes[1,1], title="Rankig per Coverage")
+).unstack(' Year of birth').plot(kind = "line", ax=axes[1,1], title="Rankig per Coverage")
 
+df.groupby(["Month day"]).agg(
+    {
+    "Ranking" : "mean",
+    #"Ranking" : "mean"
+    }
+).plot(kind = "line", legend= True, ax=axes[1,2], title="Month Day Ranking")
 
 df_delay.groupby(["Week"]).agg(
     {
@@ -129,13 +149,48 @@ df_delay.groupby(["Week"]).agg(
 ).plot(kind = "bar", ax=axes[0,2], title="Average Response Time")
 
 
-plt.title("Avergae Ranking on Comparis")
+df.groupby(["Week", " Coverage"]).agg(
+    {
+    "Ranking" : "mean",
+    #"Ranking" : "mean"
+    }
+).unstack(' Coverage').plot(kind = "bar", legend= True, ax=axes[2,0], title="Weekly Ranking development for Coverage")
+
+df.groupby(["Week day", " Coverage"]).agg(
+    {
+    "Ranking" : "mean",
+    #"Ranking" : "mean"
+    }
+).unstack(' Coverage').plot(kind = "line", legend= True, ax=axes[2,1], title="Weekday Ranking for Coverage")
+
+
+df.groupby(["Month", " Coverage"]).agg(
+    {
+    "Ranking" : "mean",
+    #"Ranking" : "mean"
+    }
+).unstack(' Coverage').plot(kind = "line", legend= True,ax=axes[2,2], title="Monthly Rankig for Coverage")
+
+plt.title("Average Ranking on Comparis")
 plt.xlabel("Date")
 plt.ylabel("Ø Ranking")
 
 
+plot_date = datetime.datetime.now().strftime("%Y-%m-%d")
+path = "/home/pi/Dokumente/Code/Crawler/plots/"
 
+#print(os.listdir(path))
+
+
+if (any(plot_date in filename for filename in os.listdir(path))):
+    print("exist")
+else:
+    plt.savefig(f"{path}crawler_{plot_date}.pdf")
+    print("file created")
+
+
+send()
 
 # Show the plot
-plt.show()
+#plt.show()
 
