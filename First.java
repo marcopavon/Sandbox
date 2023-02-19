0ab8ae0745b81e1636fdcbb16297ac4d513cbe4c
marcopavon <marco.pavoni19@yahoo.com>
2023-02-19 14:32:01 +0100

    adding JAVA

---
+++ First.java
@@ -0,0 +1,11 @@
+public class First {
+  static void myMethod() {
+    Integer x = 8;
+    String owner = "napoleon";
+    System.out.println("Hello World! "+x +owner);
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
